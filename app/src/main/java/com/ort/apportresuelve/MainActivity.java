dc3df7ecc83f193fa5842bf509772289dfde92db
Ivan Barcia <45148013@est.ort.edu.ar>
2021-08-03 22:59:13 -0300

    login comenzado y trae los reclamos

--- app/src/main/java/com/ort/apportresuelve/MainActivity.java
+++ app/src/main/java/com/ort/apportresuelve/MainActivity.java
@@ -14,209 +14,78 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import static android.content.ContentValues.TAG;
+
 public class MainActivity extends AppCompatActivity {
-    Button botonpresionado;
-
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    ArrayList<String> DatosLista;
-    ArrayList<String> DatosLista2;
-    ArrayAdapter<String> Adaptador;
-    ArrayAdapter<String> Adaptador2;
-    String textoSeleccionado;
-    String textoSeleccionado2;
-    EditText aula;
-    Button botonContinuar;
-
-    EditText mitexto;
-
-    String tipoDeReclamoSeleccionado;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
 
-
-        botonContinuar = findViewById (R.id.boton1);
-        aula = findViewById (R.id.aula);
-        mitexto = findViewById (R.id.mitexto);
-        DatosLista = new ArrayList<> ();
-        DatosLista.add ("1 Yatay");
-        DatosLista.add ("2 Rio");
-        DatosLista.add ("4 Yatay Nuevo");
-
-        Adaptador = new ArrayAdapter<String> (this, android.R.layout.simple_spinner_item, DatosLista);
-        Adaptador.setDropDownViewResource ((android.R.layout.simple_dropdown_item_1line));
-
-        Spinner spnOpciones = (Spinner) findViewById (R.id.spinner1);
-        spnOpciones.setAdapter (Adaptador);
-        spnOpciones.setOnItemSelectedListener (new AdapterView.OnItemSelectedListener () {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                textoSeleccionado = spnOpciones.getItemAtPosition (position).toString ();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-
-
-
-
-
-
-        DatosLista2 = new ArrayList<> ();
-        DatosLista2.add ("PB");
-        DatosLista2.add ("1");
-        DatosLista2.add ("2");
-        DatosLista2.add ("3");
-        DatosLista2.add ("4");
-
-        Adaptador2 = new ArrayAdapter<String> (this, android.R.layout.simple_spinner_item, DatosLista2);
-        Adaptador2.setDropDownViewResource ((android.R.layout.simple_dropdown_item_1line));
-
-        Spinner spinner2 = (Spinner) findViewById (R.id.spinner2);
-        spinner2.setAdapter (Adaptador2);
-spinner2.setOnItemSelectedListener (new AdapterView.OnItemSelectedListener () {
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        textoSeleccionado2 = spinner2.getItemAtPosition (position).toString ();
     }
 
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
-});
-
-
-
-
-
-        botonContinuar.setOnClickListener (new View.OnClickListener () {
-            @Override
-            public void onClick(View v) {
-                String edificio;
-                String aulaIngresada;
-                String piso;
-                String descripcion;
-                botonpresionado = (Button) v;
-                aulaIngresada = aula.getText ().toString ();
-                edificio = textoSeleccionado;
-                piso = textoSeleccionado2;
-                descripcion = mitexto.getText ().toString ();
-//revisar piso y edificio
-
-                Map<String, Object> registro = new HashMap<> ();
-                registro.put ("edificio", edificio);
-                registro.put ("piso", piso);
-                registro.put ("aula", aulaIngresada);
-                registro.put ("descripcion", descripcion);
-                registro.put("tipoDeReclamo", tipoDeReclamoSeleccionado);
-                db.collection ("Reclamos").add(registro).addOnSuccessListener (new OnSuccessListener<DocumentReference> () {
+    public void avanzar(View v){
+     /*  FirebaseFirestore db = null;
+        db.collection("Usuarios")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot> () {
                     @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d("Reclamos", "se creo el reclamo" + documentReference.getId ());
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
                     }
+                });*/
+        Intent intent= new Intent (v.getContext (), login.class);
+        startActivity (intent);
 
-                }).addOnFailureListener (new OnFailureListener () {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d("Reclamos", "NO se creo el reclamo"+ e);
+        //debo traer el dni y la contraseña, compararlos con el ingreso y si es correcto, loguear al usuario.
+     /*   FirebaseFirestore db = null;
+        ArrayList<usuario> nuestrosusuarios= new ArrayList<> ();
+        Task<QuerySnapshot> Usuarios=db.collection ("Usuarios").get ().addOnCompleteListener (new OnCompleteListener<QuerySnapshot> () {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                       nuestrosusuarios.add (document.toObject (usuario.class));
                     }
-                });
+                    EditText usuario=findViewById (R.id.usuario);
+                    EditText contraseña=findViewById (R.id.contraseña);
 
-                Bundle paquete;
-                paquete = new Bundle ();
-                paquete.putString ("piso", piso);
-                paquete.putString ("edificio", edificio);
-                paquete.putString ("aulaIngresada", aulaIngresada);
-                paquete.putString ("descripcion", descripcion);
-                paquete.putString("tipoDeReclamo", tipoDeReclamoSeleccionado);
-                Intent myIntent = new Intent (v.getContext (), misreclamos.class);
-                startActivity (myIntent);
-            }
-        });
-    }
+                }
+                else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
 
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
 
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.mantenimientoBoton:
-                if (checked)
-                    // selecciono mantenimiento
-                    tipoDeReclamoSeleccionado="Mantenimiento";
-                    break;
-            case R.id.tecnologiaBoton:
-                if (checked)
-                    // selecciono tecnologia
-                    tipoDeReclamoSeleccionado="Tecnologia";
-                    break;
-            case R.id.vinculosBoton:
-                if (checked)
-                    // selecciono vinculos
-                    tipoDeReclamoSeleccionado="Vinculos";
-                    break;
-        }
-    }
-
-    public void login(View v){
-        Intent loguear= new Intent (v.getContext (), login.class);
-        startActivity (loguear);
-    }
-/*
-    public void guardar(View v) {
-        String edificio;
-        String aulaIngresada;
-        String piso;
-        String descripcion;
-        botonpresionado = (Button) v;
-        aulaIngresada = aula.getText ().toString ();
-        edificio = textoSeleccionado;
-        piso = textoSeleccionado2;
-        descripcion = mitexto.getText ().toString ();
-        if (botonpresionado.getId () == R.id.mantenimientoBoton) {
-
-            Map<String, Object> registroUbicacion = new HashMap<> ();
-            registroUbicacion.put ("edificio", edificio);
-            registroUbicacion.put ("piso", piso);
-            registroUbicacion.put ("aula", aulaIngresada);
-            registroUbicacion.put ("descripcion", descripcion);
-            bdd.child ("mantenimiento").push ().setValue (registroUbicacion);
-
-        } else if (botonpresionado.getId () == R.id.vinculosBoton) {
-            Map<String, Object> registroUbicacion = new HashMap<> ();
-            registroUbicacion.put ("edificio", edificio);
-            registroUbicacion.put ("piso", piso);
-            registroUbicacion.put ("aula", aulaIngresada);
-            registroUbicacion.put ("descripcion", descripcion);
-            bdd.child ("vinculos").push ().setValue (registroUbicacion);
-        } else {
-            Map<String, Object> registroUbicacion = new HashMap<> ();
-            registroUbicacion.put ("edificio", edificio);
-            registroUbicacion.put ("piso", piso);
-            registroUbicacion.put ("aula", aulaIngresada);
-            registroUbicacion.put ("descripcion", descripcion);
-            bdd.child ("tecnologia").push ().setValue (registroUbicacion);
         }
 
+
+
+
+    });
+      Intent intent= new Intent (v.getContext (), MainActivity.class);
+      startActivity (intent);
+
+      */
     }
- */
     }
